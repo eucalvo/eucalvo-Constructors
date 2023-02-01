ddd5fae730b34dae51bbfd5939af4a0aadb449ef
eucalvo <eliamcalvo24@gmail.com>
2023-02-01 04:50:32 +0000

    Completed Lab Constructors

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -31,7 +31,8 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
-
+        level = 1;
+        this.playerName = playerName;
     }
 
     /**
@@ -48,7 +49,8 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
-
+        this.playerName = playerName;
+        this.level = level;
     }
 
 }
